22be9e25be2256c05615ab254958037026c54787
Hila05 <hila.grinerr@gmail.com>
2020-11-09 15:51:30 +0200

    focus

---
+++ any-access.js
@@ -0,0 +1,10 @@
+// $('.btn-block').click(function () {
+//     setTimeout(function () {
+//         console.log('hh')
+//         $('input.form-control:eq(0)').focus();
+//     }, 300);
+// })
+
+$('.btn.btn-primary').on('click', function() {
+    $('#firstName').focus();
+});
